d447da5b6ae14faeba3ef9251b76389e326f74aa
Ansh Raj <itspleasure7194@gmail.com>
2026-01-01 20:46:12 +0530

    Add files via upload

--- js/check.js
+++ js/check.js
@@ -1,1 +1,33 @@
+const navToggle = document.getElementById("navToggle");
+const mobileNav = document.getElementById("mobileNav");
+const icon = navToggle.querySelector("i");
 
+navToggle.addEventListener("click", () => {
+  mobileNav.classList.toggle("open");
+  icon.classList.toggle("fa-bars");
+  icon.classList.toggle("fa-xmark");
+});
+  const overlay = document.getElementById("siteDisclaimer");
+  const btn = document.getElementById("disclaimerBtn");
+  const countdownEl = document.getElementById("countdown");
+
+  let timeLeft = 10;
+
+  const timer = setInterval(() => {
+    timeLeft--;
+    countdownEl.textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+      closeDisclaimer();
+    }
+  }, 1000);
+
+  function closeDisclaimer() {
+    clearInterval(timer);
+    overlay.classList.add("hide");
+    setTimeout(() => {
+      overlay.style.display = "none";
+    }, 400);
+  }
+
+  btn.addEventListener("click", closeDisclaimer);
